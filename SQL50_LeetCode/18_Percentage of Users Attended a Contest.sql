16ae6ce2ea16213fdbced5cf92cfb0915dd6fff0
Ashutosh Singh <asks.it10@gmail.com>
2026-01-19 17:40:45 +0000

    solved more questions

---
+++ SQL50_LeetCode/18_Percentage of Users Attended a Contest.sql
@@ -0,0 +1,4 @@
+select r.contest_id, round((count(r.user_id) / (select count(user_id) from Users)) * 100, 2) as percentage
+from Register r 
+group by r.contest_id
+order by percentage desc, r.contest_id;
