7ff3678f7af3c62528fd6e4c447d6f7bb79c5818
Ashutosh Singh <asks.it10@gmail.com>
2026-01-21 18:16:39 +0000

    changes in solution

--- SQL50_LeetCode/21_Immediate Food Delivery.sql
+++ SQL50_LeetCode/21_Immediate Food Delivery.sql
@@ -4,3 +4,12 @@ from Delivery
 where (customer_id, order_date) in (
 select customer_id, min(order_date) from Delivery group by customer_id
 );
+
+
+-- Solution With Common Table Expression (CTE)
+-- with cte as(select *, row_number() over(partition by customer_id order by order_date) as rn, case when order_date = customer_pref_delivery_date then 'immediate' else 'scheduled' end as type
+-- from delivery)
+
+-- select round(100*sum(type = 'immediate')/count(customer_id),2) as immediate_percentage
+-- from cte
+-- where rn = 1
